95798a89fb6f2baa05f8bc73ddbb8a1f6c643a09
cleiltont <cleiltonceni2000@gmail.com>
2020-06-10 18:48:25 -0300

    Projeto concluido

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # spatt
 Store projects and their tasks
 
+## Sobre
+É um CRUD feito em NodeJs.
+Ele armazena projetos e suas tarefas.
+
+## Conhecimento obtido
+- **Methods:**
+	- GET = Para lista os projeto
+	- POST = Para criar novos projetos e tarefas
+	- PUT = Para altera no do projeto dados do projeto
+	- DELETE = Para excluir projeto
+
+- **Middlewares:**
+	- Verica se o projeto já existe.
+	- Verifica cada requisição e mostra o tempo da resposta.
--- src/server.js
+++ src/server.js
@@ -3,7 +3,27 @@ const server = express();
 
 server.use(express.json());
 
+// Mostra o tempo de cada requisição
+server.use((req, res, next) => {
+	console.time('Tempo');
+
+	next();
+
+	console.timeEnd('Tempo');
+});
+
 const projects = [];
+
+// Verifica se o projeto ja existe
+function checkProjectExist(req, res, next){
+	const { id } = req.params;
+	
+	if(!projects[id]){
+		return res.status(400).json({ message: 'Project does not exist'});
+	}
+
+	return next();
+}
 
 // Cria novo projeto
 server.post('/projects', (req, res) => {
@@ -13,13 +33,23 @@ server.post('/projects', (req, res) => {
 	return res.json(projects);
 })
 
+// Cria nova tarefa
+server.post('/projects/:id/tasks', (req, res) => {
+	const { id } = req.params;
+	const { title } = req.body;
+
+	projects[id].tasks.push(title);
+
+	return res.json(projects);
+});
+
 // Lista todos dos projetos
 server.get('/projects', (req, res) => {
 	return res.json(projects);
 });
 
 // Altera titulo do projeto
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', checkProjectExist, (req, res) => {
 	const { id } = req.params;
 	const { title } = req.body;
 
@@ -29,7 +59,7 @@ server.put('/projects/:id', (req, res) => {
 });
 
 // Exclui projeto
-server.delete('/projects/:id', (req, res, next) => {
+server.delete('/projects/:id', checkProjectExist, (req, res, next) => {
 	const { id } = req.params;
 
 	projects.splice(id, 1);
